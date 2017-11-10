cc76d970e06ab86deadce481561c575f49764ea0
Emanuele Gian <emanuele.gian@quinary.com>
2017-11-10 12:38:43 +0100

    Now dateFixer.py functions as command line scripts Update Readme.md

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This python script run only on macOsX using the "SetFile" command form xCode.
 Use readExifTax.py to print tag from file on a specific folder.
 
 (see inside the scripts for configuration)
+
+Code inspired from:
+http://bastibe.de/2015-10-03-changing-file-creation-dates.html
+https://stackoverflow.com/questions/4764932/in-python-how-do-i-read-the-exif-data-for-an-image 
+https://srcco.de/posts/writing-python-command-line-scripts.html
--- dateFixer.py
+++ dateFixer.py
@@ -1,27 +1,63 @@
+import logging
+from argparse import ArgumentParser
 import os,sys
 import PIL
 import datetime
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-fotodir = 'foto'
+ExifDateTag = 'DateTimeOriginal'
 
 
-def get_field (exif,field) :
+def get_field(exif,field):
     for (k,v) in exif.iteritems():
         if TAGS.get(k) == field:
             return v
 
 
-for filename in os.listdir('foto'):
+def main(args):
 
-    exifCreateDate = get_field(PIL.Image.open(fotodir + '/' + filename)._getexif(), 'DateTimeOriginal')
+    ExifDateTag = 'DateTimeOriginal'
 
-    if exifCreateDate != None :
-        print 'Change date on file : ' + filename + ' to ' + exifCreateDate;
-        d = datetime.datetime.strptime(exifCreateDate, '%Y:%m:%d %H:%M:%S')
-        # set the file "modification time"
-        os.system('SetFile -d "{}" {}'.format(d.strftime('%m/%d/%Y %H:%M:%S'), fotodir + '/' + filename))
-        # set the creation time
-        os.system('SetFile -m "{}" {}'.format(d.strftime('%m/%d/%Y %H:%M:%S'), fotodir + '/' + filename))
+    fotodir = args.folder
 
+    if args.dry_run:
+        print '--- dry-run mode, no file will be change ---'
+
+    for filename in os.listdir(fotodir):
+
+        print 'File : ' + filename
+
+        t = datetime.datetime.fromtimestamp(os.path.getmtime(fotodir + '/' + filename))
+        tnotime = datetime.datetime(t.year, t.month, t.day, 0, 0, 0)
+        print 'File Creation Time: %s' % t
+
+        exifcreatedate = get_field(PIL.Image.open(fotodir + '/' + filename)._getexif(), ExifDateTag)
+        exifdatenotime = None
+
+        if exifcreatedate is not None:
+            exifcreatedateString = datetime.datetime.strptime(exifcreatedate, '%Y:%m:%d %H:%M:%S')
+            exifdatenotime = datetime.datetime(exifcreatedateString.year, exifcreatedateString.month, exifcreatedateString.day, 0, 0, 0)
+
+        print 'Exif ' + ExifDateTag + ': %s' % exifcreatedate
+
+        if exifcreatedate is not None and tnotime != exifdatenotime:
+            print 'Date differs, so change File Creation Time equal to ' + ExifDateTag
+
+            if args.dry_run is False:
+                d = datetime.datetime.strptime(exifcreatedate, '%Y:%m:%d %H:%M:%S')
+                print 'Change file creation time and modification time to %s' % d
+                # set the creation time
+                os.system('SetFile -m "{}" {}'.format(d.strftime('%m/%d/%Y %H:%M:%S'), fotodir + '/' + filename))
+                # set the file "modification time"
+                os.system('SetFile -d "{}" {}'.format(d.strftime('%m/%d/%Y %H:%M:%S'), fotodir + '/' + filename))
+
+        print '---------------------------------------------------------------------'
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--dry-run', help='Noop, do not write anything', action='store_true')
+    parser.add_argument('folder', help='Path to folder to scan for images')
+    args = parser.parse_args()
+    main(args)
